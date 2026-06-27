3a9d99d51a929ab30e7252e86661a7c68bd3ae06
shrenika2 <ShrenikaPatil0211@gmail.com>
2026-06-28 01:20:05 +0530

    Time: 0 ms (100%), Space: 12.1 MB (45.29%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -3,32 +3,18 @@ public:
     int countBattleships(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
-        int dr[4]={-1 , 0 , 1 , 0 };
-        int dc[4]={0 , 1 , 0 , -1};
-        queue<pair<int ,int>>q;
-        int cnt = 0 ;
+
+        int ans = 0 ;
+
         for (int i = 0 ; i < n ; i++){
             for (int j = 0 ; j < m ; j++){
-                if(board[i][j]=='X'){
-                    cnt++;
-                    q.push({i , j});
-                    while(!q.empty()){
-                        auto[r , c]=q.front();q.pop();
-                        for (int i = 0 ; i < 4 ; i++){
-                            int nr = r + dr[i];
-                            int nc = c + dc[i];
+                if(board[i][j]=='.') continue ;
+                if(i > 0 && board[i-1][j]=='X') continue ;
+                if(j>0 && board[i][j-1]=='X') continue ;
 
-                            if(nr >= 0 && nc >= 0 && nr<n && nc<m && board[nr][nc]=='X'){
-                                board[nr][nc]='.';
-                                q.push({nr , nc});
-                            }
-                        }
-                        
-                    }
-                }
+                ans++;
             }
-        }
-        return cnt ;
+        }return ans ;
         
     }
 };
