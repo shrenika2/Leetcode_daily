3a8100573b71df04f5fc72eb58939ec347703a45
shrenika2 <ShrenikaPatil0211@gmail.com>
2026-07-03 01:50:26 +0530

    Time: 3 ms (53.12%), Space: 28.3 MB (49.99%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
-int dfs(int r , int c , vector<vector<int>>& grid , vector<vector<bool>> & vis){
-    vis[r][c]=1;
-      int dr[4]={-1 , 0 , 1 , 0};
-        int dc[4]={0 , 1 , 0 , -1};
-        int area = 1 ;int n=grid.size();
-    int m=grid[0].size();
-    for(int i = 0 ; i < 4 ; i++){
-        int nr = r + dr[i];
-        int nc= c+dc[i];
-       if(nr>=0 && nr<n &&
-           nc>=0 && nc<m &&
-           !vis[nr][nc] &&
-           grid[nr][nc]==1){
+int mx = 0 ;
+int cnt = 0;
 
-            area+=dfs(nr,nc,grid,vis);
-        }
+
+    void dfs(int r , int c , vector<vector<int>>& grid ){
+         int dr[4]={0 , -1 , 0 , 1};
+        int dc[4]={-1 , 0 , 1 , 0};
+      
+         int n = grid.size();
+        int m = grid[0].size();
+       
+        for (int k = 0 ; k < 4 ; k++){
+                        int nr = r + dr[k];
+                        int nc = c + dc[k];
+                        if(nr < n && nc < m && nr>=0 && nc >= 0 && grid[nr][nc]==1){
+                            cnt++;
+                            grid[nr][nc]=0;
+                            dfs(nr , nc , grid);
+                        }
+                    }
+        
     }
-    return area;
-}
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int n = grid.size();
-        int m = grid[0].size(); 
-        vector<vector<bool>>vis(n , vector<bool>(m , 0));
-     
-int ans = 0 ;
+        int m = grid[0].size();
+      
+        vector<vector<bool>> vis(n , vector<bool>(m , 0));
         for (int i = 0 ; i < n ; i++){
             for (int j = 0 ; j < m ; j++){
-                if(grid[i][j]==1 && !vis[i][j]){
-
-                ans=max(ans,dfs(i,j,grid,vis));
-            }
+                if(grid[i][j]==1){
+                   cnt = 1;
+                    grid[i][j] = 0;
+                    dfs(i, j, grid);
+                    mx = max(mx, cnt);
             }
         }
-        return ans ;
+        }
+        return mx ;
+        
     }
 };
