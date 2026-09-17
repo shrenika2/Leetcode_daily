c127051301bc8b97bd71f13046710d998e9c0432
shrenika2 <ShrenikaPatil0211@gmail.com>
2026-09-17 20:06:59 +0530

    Time: 91 ms (15.14%), Space: 113.3 MB (20.43%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st(nums.begin() , nums.end());
+        return nums.size()> st.size();
+        
+    }
+};
