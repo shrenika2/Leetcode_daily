eed46bd52a7fdf7a79c1e18f59fe0a7bc622e23c
shrenika2 <ShrenikaPatil0211@gmail.com>
2026-09-17 18:41:28 +0530

    Time: 4 ms (53.13%), Space: 15 MB (28.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,38 +1,16 @@
-// brute 
-// tc = o(n^2)
-// sc = o(1)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        int i = 0 ;
-        while(i< n ){
-            int req = target - nums[i];
-            for (int j = i+1 ; j < n ; j++){
-                if(nums[j]==req){
-                    return {i , j};
-                    break;
-                }
+        unordered_map<int , int> mp ;
+
+        for (int i = 0 ; i < n ; i++){
+            int rem = target - nums[i];
+            if(mp.count(rem)){
+                return {i , mp[rem]};
             }
-            i++;
+            mp[nums[i]]=i;
         }
-        return {};
+        return {} ;
     }
 };
-// optimal 
-// tc = o(n)
-// sc = o(n)
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         unordered_map<int,int> mp;
-
-//         for(int i = 0; i < nums.size(); i++) {
-//             int req = target - nums[i];
-//             if(mp.count(req))
-//                 return {mp[req], i};
-//             mp[nums[i]] = i;
-//         }
-//         return {};
-//     }
-// };
