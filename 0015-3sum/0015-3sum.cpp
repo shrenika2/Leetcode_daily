fede45373502d1e01c4180f69626d2c320510700
shrenika2 <ShrenikaPatil0211@gmail.com>
2026-09-25 13:06:50 +0530

    Time: 43 ms (82.64%), Space: 29.1 MB (45.57%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,15 +7,15 @@ public:
         for (int i = 0 ; i < n-2 ; i++){
              if(i > 0 && nums[i]==nums[i-1]) continue;
      
-            if(nums[1]>0) break;
+            if(nums[i]>0) break;
             int f = i+1 ;
             int l = n-1 ;
             while(f <l){
                 int sum = nums[i]+nums[f]+nums[l];
                if(sum == 0){
                     res.push_back({nums[i] , nums[f] , nums[l]});
-                     if( f <l && nums[f]==nums[f+1]) f++;
-                if( f<l && nums[l]==nums[l-1]) l--;
+                     while( f <l && nums[f]==nums[f+1]) f++;
+                while( f<l && nums[l]==nums[l-1]) l--;
                     f++;
                     l--;
                 }else if (sum < 0){
